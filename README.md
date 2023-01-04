7a90809b4788d0c31ebbce90469175d9e2c780a3
elvonking <elvisotwoma@gmail.com>
2023-01-04 19:30:28 +0300

    project upload

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
-image processing api
+This is my first submission for the image processing api in the udacity full stack javascript developer nanodegree.
+Existing image file names are the default images provided in the project challenge.
+
+##### You can edit the filename parameter to confirm that the api works for the following images:
 
 1. http://localhost:3000/api?filename=fjord&width=300&height=400
+2. http://localhost:3000/api?filename=encenada&width=300&height=400
+3. http://localhost:3000/api?filename=icelandwaterfall&width=300&height=400
+4. http://localhost:3000/api?filename=palmtunnel&width=300&height=400
+5. http://localhost:3000/api?filename=santamonica&width=300&height=400
--- src/tests/utilitiesSpec.ts
+++ src/tests/utilitiesSpec.ts
@@ -1,42 +1,42 @@
-import ImageUtilities from '../utilities/utilities';
+import ImageToolbox from '../utilities/utilities';
 import { imagesPath, thumbImagesPath, fileExtension } from '../utilities/utilities';
 
 describe('utilities tests', () => {
   it('should get a valid response for file names in root folder', () => {
     const filename = 'fjord';
-    expect(ImageUtilities.isFileName(filename)).toBeTruthy;
+    expect(ImageToolbox.isFileName(filename)).toBeTruthy;
     const filename2 = 'nonexistent';
-    expect(ImageUtilities.isFileName(filename2)).toBeFalsy;
+    expect(ImageToolbox.isFileName(filename2)).toBeFalsy;
   });
 });
 
 describe('is valid numbers', () => {
   it('should check parameters provided are positive integers', () => {
-    expect(ImageUtilities.isRealNumber('10')).toBeTruthy;
-    expect(ImageUtilities.isRealNumber('-19')).toBeFalsy;
-    expect(ImageUtilities.isRealNumber('r2d2')).toBeFalsy;
+    expect(ImageToolbox.isRealNumber('10')).toBeTruthy;
+    expect(ImageToolbox.isRealNumber('-19')).toBeFalsy;
+    expect(ImageToolbox.isRealNumber('r2d2')).toBeFalsy;
   });
 });
 
 describe('create paths', () => {
   it('should return a path to an unprocessed image before processing has occurred', () => {
-    const string = ImageUtilities.createFilePath('raw');
+    const string = ImageToolbox.createFilePath('raw');
     expect(string).toBe(`${imagesPath}\\raw${fileExtension}`);
   });
 
   it('should return a path to a thumb image named processed500500.jpg', () => {
-    const string = ImageUtilities.createThumbFilePath('processed', '500', '500');
+    const string = ImageToolbox.createThumbFilePath('processed', '500', '500');
     expect(string).toBe(`${thumbImagesPath}\\processed500500${fileExtension}`);
   });
 
   it('should complete processing a file', async () => {
     await expectAsync(
-      ImageUtilities.createThumbFile('testImage', '200', '200')
+      ImageToolbox.createThumbFile('testImage', '200', '200')
     ).toBeResolved();
   });
 
   it('should process  file and notify the user', async () => {
-    const processingMessage: string = await ImageUtilities.createThumbFile(
+    const processingMessage: string = await ImageToolbox.createThumbFile(
       'picha',
       '350',
       '450'
@@ -49,6 +49,6 @@ describe('create paths', () => {
 
 describe('is thumb file', () => {
   it('should return false if the file exists', () => {
-    expect(ImageUtilities.isThumbFIle()).toBeFalsy;
+    expect(ImageToolbox.isThumbFIle()).toBeFalsy;
   });
 });
